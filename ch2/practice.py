8f42dc366d458d0ecd61d8c86ab9add88098e69f
eun-e <eun2cho@ewha.ac.kr>
2026-01-01 17:32:13 +0900

    Update practice.py

--- ch2/practice.py
+++ ch2/practice.py
@@ -1,24 +1,27 @@
-# 특수 함수로 텐서 생성
+import torch
 
-zeros_tensor = torch.zeros((3,3))
-ones_tensor = torch.ones((2,4))
-full_tensor = torch.full((2,2), 7) 
+def main():
+        # 특수 함수로 텐서 생성
+        zeros_tensor = torch.zeros((3,3))
+        ones_tensor = torch.ones((2,4))
+        full_tensor = torch.full((2,2), 7) 
+        
+        arange_tensor = torch.arange(0,10, step=2)  
+        linspace_tensor = torch.linspace(0,1, steps=5)  # 중요 포인트!! step은 간격, steps는 개수 의미
 
-arange_tensor = torch.arange(0,10, step=2)  
-linspace_tensor = torch.linspace(0,1, steps=5)  # 중요 포인트!! step은 간격, steps는 개수 의미
+        print("zeros_tensor:\n", zeros_tensor)
+        print("ones_tensor:\n", ones_tensor)
+        print("full_tensor:\n", full_tensor)
+        print("arange_tensor:\n", arange_tensor)
+        print("linspace_tensor:\n", linspace_tensor)
 
-tensor([[0., 0., 0.],
-        [0., 0., 0.],
-        [0., 0., 0.]])
-tensor([[1., 1., 1., 1.],
-        [1., 1., 1., 1.]])
-tensor([[7, 7],
-        [7, 7]])
-tensor([0, 2, 4, 6, 8])
-tensor([0.0000, 0.2500, 0.5000, 0.7500, 1.0000])
+        
+        # 무작위 텐서 생성
+        rand_tensor = torch.rand((3,3))
+        randn_tensor = torch.randn((2,2))        # 평균이 0, 표준편차가 1인 정규 분포
+        
+        print("rand_tensor:\n", rand_tensor)
+        print("randn_tensor:\n", randn_tensor)
 
-=================================================================================================
-# 무작위 텐서 생성
-
-rand_tensor = torch.rand((3,3))
-randn_tensor = torch.randn((2,2))        # 평균이 0, 표준편차가 1인 정규 분포
+if __name__ == "__main__":
+    main()
