5d080373ab395f3d69bbf917850941391c4ffb2b
eun-e <eun2cho@ewha.ac.kr>
2026-01-01 17:11:23 +0900

    Fix typos in tensor creation functions

--- ch2/practice.py
+++ ch2/practice.py
@@ -4,5 +4,5 @@ zeros_tensor = torch.zeros((3,3))
 ones_tensor = torch.ones((2,4))
 full_tensor = torch.full((2,2), 7)  # 2X2 텐서를 7로 전부 채움
 
-arrange_tensor = torch.arrange(0,10, step=2)   # 0, 2, 4, 6, 8
-linspace_tensor = torch.linspace(0,1, step=5)  # 0, 0.25, 0.5, 0.75, 1
+arange_tensor = torch.arange(0,10, step=2)   # 0, 2, 4, 6, 8
+linspace_tensor = torch.linspace(0,1, steps=5)  # 0, 0.25, 0.5, 0.75, 1
