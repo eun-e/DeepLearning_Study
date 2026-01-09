c7562d3577d173684269ad812ac5b1986fd1e190
eun-e <eun2cho@ewha.ac.kr>
2026-01-09 13:15:35 +0900

    Update practice1.py

--- ch5/practice1.py
+++ ch5/practice1.py
@@ -1,4 +1,5 @@
 # Sobel Filter로 이미지 내에서 색이 갑자기 변하는 경계선을 찾아내는 역할을 함
+
 filter_2d = torch.tensor([[
     [1.0, 0.0, -1.0],
     [1.0, 0.0, -1.0],
@@ -6,6 +7,14 @@ filter_2d = torch.tensor([[
 ]]).unsqueeze(0)
 
 sample_image_2 = torch.zeros(1,1,8,8)
-sample_image_2[0.0.:,3:5] = 1.0   # 3~4열만 하얀색(1.0)으로 칠한 세로 기둥 모
-
+sample_image_2[0.0.:,3:5] = 1.0   # 3~4열만 하얀색(1.0)으로 칠한 세로 기둥 모양
 output_2 = nn.functional.conv2d(sample_image_2, filter_2d)
+
+
+# Stride와 Padding
+import torch.nn.functional as F
+
+input_image = torch.randn(1,1,6,6)    # 1개의 배치, 1개의 채널, 6X6 크기
+filter = torch.randn(1,1,3,3)
+output = F.conv2d(input_image, filter, stride=1, padding=1)
+
